d1b5b6e205da709e40a7f145eddae774ba7e8ffb
Wilson Felipe <wfelipe2011@gmail.com>
2021-01-05 16:40:43 -0300

    Fundo interativo

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function carregar() {
     var hora = data.getHours()
     var min = data.getMinutes()
     //Teste funcionalidade
-    //var hora = 8
+    
     
     if (hora >= 0 && hora < 12){
         msg.innerHTML = `Bom dia! <br>Agora são ${hora} horas e ${min} minutos.`//
         imagem.src = 'imagens/manha.png'
-        document.body.style.background ='#b6d1da'
+        document.body.style.background = 'url("https://source.unsplash.com/1600x900/?nature")'
     }else if(hora >=12 && hora <= 18){
         msg.innerHTML = `Boa tarde!<br> Agora são ${hora} horas e ${min} minutos.`//
         imagem.src = 'imagens/tarde.png'
-        document.body.style.background ='#d4824b'
+        document.body.style.background = 'url("https://source.unsplash.com/1600x900/?nature")'
     }else{
         msg.innerHTML = `Boa noite! <br> Agora são ${hora} horas e ${min} minutos.`//Boa noite
         imagem.src = 'imagens/noite.png'
-        document.body.style.background ='#03454e'
+        document.body.style.background = 'url("https://source.unsplash.com/1600x900/?nature")'
     }
 }
